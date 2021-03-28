6cf5ac872ddc4a1dfe76dde9b17f5d7c3c6a424d
Shah Darshil <darshilshah15575@gmail.com>
2021-03-28 18:19:03 +0530

    Added code to use the user routes for handling the request through express library.

--- application.js
+++ application.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const userRoutes = require('./routes/user.js');
+
 const app = express();
 app.use(bodyParser.json());
 
-
+app.use('/user', userRoutes);
 
 app.listen(8080);
