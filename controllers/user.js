68335652520f36b64b07ee700c0a445620df9cfb
Shah Darshil <darshilshah15575@gmail.com>
2021-04-01 10:05:18 +0530

    Added functions authenticateUser() to authenticate user by sending OTP to user via email to verify their account and insertAuthenticateDetails() to insert the sent OTP into the database.
    Also modified the queries to take the full qualified column name
     e.g:
        previous: SELECT name FROM User;
        now: SELECT User.name FROM User;

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,105 @@
 const db = require('../database/playoff.js');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+const passwordManager = require(__dirname + '/../passwords/password.js');
+const utils = require(__dirname + '/../utils.js');
+
+exports.authenticateUser = (req, res, next) => {
+
+    const email = req.body.email;           // Email address of the user that want to register
+
+    // Creating a service to send email through gmail server.
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'playoff1322@gmail.com',
+            pass: passwordManager.getMailPassword
+        }
+    });
+
+    // Generate an OTP
+    const oneTimePassword = utils.generateOTP();
+
+    // Initializing the header and body for email to send.
+    const mailOptions = {
+        from: 'playoff1322@gmail.com',
+        to: email,
+        subject: 'One time password',
+        text: '',
+        html: '<b>' + oneTimePassword + '</b> is your OTP and is valid for <b>5 minutes</b><br/><br/><p>We have sent you a code to verify your account.</br> It is to ensure that only you have access to your account.</br> After verifying you can access your playoff account.</p>'
+    };
+
+    // Sending the mail to the user.
+    transporter.sendMail(mailOptions).then(result => {
+        if(insertAuthenticationDetails(email, oneTimePassword)) {
+            res.status(200).json({
+                message: 'success'
+            });
+        } else {
+            throw TypeError('Failed to store in the database');
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json({
+            message: 'failure'
+        });
+    });
+}
+
+const insertAuthenticationDetails = function(username, oneTimePassword) {
+    let timestamp = utils.generateUnixTimestamp();          // Generating the UNIX timestamp for the users temperory OTP.
+
+    // Inserting the record in the database with the generated OTP for the user or Updating the record in case if already exist.
+    db.query("INSERT INTO AuthenticateUser (AuthenticateUser.name, AuthenticateUser.otp, AuthenticateUser.otp_expiration) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE otp = ?, otp_expiration = ?", [username, oneTimePassword, timestamp, oneTimePassword, timestamp])
+    .then(result => {
+        return true;
+    })
+    .catch(err => {
+        console.log(err);
+        return false;
+    });
+    return true;
+}
 
 exports.createUserProfile = (req, res, next) => {
     // Fields retrieved from the request body sent by the user.
-    const name = req.body.name;             // name of the user.
-    const password = req.body.password;     // password is the hashed string sent from the client/user.
-    const location = req.body.location;     // location is the actual location of user that he entered.
+    const username = req.body.name;                          // username of the user.
+    const password = req.body.password;                      // password is the hashed string sent from the client/user.
+    const location = req.body.location;                      // location is the actual location of user that he entered.
+    const oneTimePassword = req.body.oneTimePassword;        // OTP is the value used passed by user to verify his/her account.
 
     // Hashing the password to store it in the database.
     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
 
-    // Inserting the user registration details in the database.
-    db.query("INSERT INTO User (name, password, location) values (?, ?, ?)", [name, hashedPassword, location])
-    .then(result => {
-        // Returning the response with the success message with status code 201.
-        return res.status(201).json({
-            message: 'successfully registered',
-            user: { name: name, location: location }
-        });
+    // Getting the user from database to compare it with the OTP obtain from the user and checking whether or not if it's expired.
+    db.query("SELECT AuthenticateUser.name 'username' FROM AuthenticateUser WHERE AuthenticateUser.name = ? AND AuthenticateUser.otp = ? AND otp_expiration >= UNIX_TIMESTAMP()", [username, oneTimePassword])
+    .then(([result, constraint]) => {
+        if(result[0].username) {
+            // Inserting the user registration details in the database.
+            db.query("INSERT INTO User (User.name, User.password, User.location) values (?, ?, ?)", [username, hashedPassword, location])
+            .then(result => {
+                // Returning the response with the success message with status code 201.
+                return res.status(201).json({
+                    message: 'successfully registered',
+                    user: { name: username, location: location }
+                });
+            })
+            .catch(err => {
+                // Returning the error response of 501 in case of the failure.
+                console.log(err);
+                res.status(500).json({
+                    message: 'username already exists'
+                });
+            });
+        }
     })
     .catch(err => {
-        // Returning the error response of 501 in case of the failure.
         console.log(err);
+        // Returning the error response of 501 in case of the failure.
         res.status(500).json({
-            message: 'username already exists'
+            message: 'OTP expired. Please try again.'
         });
     });
 }
@@ -38,7 +114,7 @@ exports.validateUserProfile = (req, res, next) => {
     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
 
     // Validating the user with its respected username and password.
-    db.execute("SELECT name, password, location FROM User WHERE name = ? AND password = ?", [name, hashedPassword])
+    db.execute("SELECT User.name, User.password, User.location FROM User WHERE User.name = ? AND User.password = ?", [name, hashedPassword])
     .then(([rows, constraints]) => {
         // Checking if the data provided by the user in the request body
         // matches the data stored in the actual database and is not empty.
@@ -55,6 +131,7 @@ exports.validateUserProfile = (req, res, next) => {
     })
     .catch(err => {
         // Returning the error response code of 404 in case of some failure.
+        // This code will execute even if the password is not found in the database.
         console.log(err);
         return res.status(404).json({
             message: 'incorrect username or password'
