685f111449270898f5cd0afcfa0f022c99bdb1f6
Shah Darshil <darshilshah15575@gmail.com>
2021-04-02 12:07:39 +0530

    Added the missing password field to pass as arguments to prepared statement in deleteUser function.

--- controllers/user.js
+++ controllers/user.js
@@ -106,7 +106,7 @@ exports.deleteUser = (req, res, next) => {
     const email = req.body.email;
     const password = crypto.createHash('sha256').update(req.body.password).digest('hex');
 
-    db.query("DELETE FROM User WHERE email = ? AND password = ?", [email])
+    db.query("DELETE FROM User WHERE email = ? AND password = ?", [email, password])
     .then(result => {
         if(result[0].affectedRows > 0) {
             res.status(201).json({
