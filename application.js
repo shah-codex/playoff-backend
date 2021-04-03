7bce10c736d324e411a46676f8030070a20f96d0
Shah Darshil <darshilshah15575@gmail.com>
2021-04-03 17:02:30 +0530

    Imported the teamRoutes to user the new team.js routes for listening and redirecting the user on the properitery route for team related operations.

--- application.js
+++ application.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user.js');
+const teamRoutes = require('./routes/team.js');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
+
+app.use('/team', teamRoutes);
 
 app.listen(8080);
