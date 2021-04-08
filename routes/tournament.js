27ca47d52f2509d193422ed7d13515ecb47065f2
Shah Darshil <darshilshah15575@gmail.com>
2021-04-08 09:03:28 +0530

    Added routes tournament.js for tournaments. Includes CRUD operation on the tournaments.

---
+++ routes/tournament.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const tournamentController = require(__dirname + '/../controllers/tournament.js');
+
+router.put('/create', tournamentController.createTournament);
+
+router.post('/join', tournamentController.joinTournament);
+
+router.delete('/delete', tournamentController.deleteTournament);
+
+router.post('/update', tournamentController.updateTournament);
+
+router.get('/:location', tournamentController.getTournaments);
+
+router.get('/:location/:tournament', tournamentController.getTournament);
+
+module.exports = router;
