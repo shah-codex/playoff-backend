63a9c0e4ab2da5fc58be197af384980d0c9919d6
Shah Darshil <darshilshah15575@gmail.com>
2021-04-08 09:07:07 +0530

    Added a route to use for tournaments starting from /tournament after the IP of the server.
    Imported the tournament routes to execute when the requested URL be like 
    x.x.x.x:xx/tournament/ and so on.

--- application.js
+++ application.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user.js');
 const teamRoutes = require('./routes/team.js');
+const tournamentRoutes = require('./routes/tournament.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,5 +11,7 @@ app.use(bodyParser.json());
 app.use('/user', userRoutes);
 
 app.use('/team', teamRoutes);
+
+app.use('/tournament', tournamentRoutes);
 
 app.listen(8080);
