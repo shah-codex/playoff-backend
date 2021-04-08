056b78216bafc13ec29fd302d6201b3eb01cd417
Shah Darshil <darshilshah15575@gmail.com>
2021-04-08 09:25:11 +0530

    Updated the application.js of REST service to allow control over the connecting IP location, type of methods they can use while requesting or posting a resource and allowing the client to set the headers type.

--- application.js
+++ application.js
@@ -8,6 +8,13 @@ const tournamentRoutes = require('./routes/tournament.js');
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/user', userRoutes);
 
 app.use('/team', teamRoutes);
